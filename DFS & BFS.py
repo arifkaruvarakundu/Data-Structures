65f73f38495ddaee1cd82283f9bf68cb62eaba09
MUHAMMED HARIF N <128890173+arifkaruvarakundu@users.noreply.github.com>
2025-11-16 16:28:30 +0300

    Refactor DFS and BFS to use list for visited
    Changed visited from a set to a list for both DFS and BFS functions.

--- DFS & BFS.py
+++ DFS & BFS.py
@@ -1,31 +1,29 @@
 #---------- DFS -------------#
 
-def dfs(graph,start):
-    visited = set()
+def dfs(graph, start):
+    visited = []
     stack = [start]
 
     while stack:
         vertex = stack.pop()
-
         if vertex not in visited:
-            visited.add(vertex)
+            visited.append(vertex)
             stack.extend([v for v in graph[vertex] if v not in visited])
-
     return visited
-
+    
 #------------------ BFS ---------------------------#
  
-def bfs(graph,start):
-    visited = set()
+def bfs(graph, start):
+    visited = []
     queue = [start]
 
     while queue:
         vertex = queue.pop(0)
-
         if vertex not in visited:
-            visited.add(vertex)
-            queue.extend([v for v in graph[vertex] if v not in visited])
-
+            visited.append(vertex)
+            for v in graph[vertex]:
+                if v not in visited and v not in queue:
+                    queue.append(v)
     return visited
 
 
